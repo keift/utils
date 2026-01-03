259434c7f84763978a71d3e2cb515cd996bcab39
fir4tozden <fir4tozden@gmail.com>
2026-01-03 05:44:58 +0300

    v1.0.0

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# Keift Utils
+[String]: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String
+[Number]: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number
+[Boolean]: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Boolean
+[Object]: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object
+[Date]: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
+[Buffer]: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/ArrayBuffer
+[Promise]: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise
+[Void]: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Undefined
+[Null]: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/null
+[Undefined]: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Undefined
+
+<!---->
+
+<div align="center">
+  <br/>
+  # Keift Utils
+  <br/>
+  <br/>
+  <img src="https://img.shields.io/npm/v/typof?label=version&color=%23633BFF"/>
+  <img src="https://img.shields.io/npm/l/typof?label=license&color=%23633BFF"/>
+  <img src="https://img.shields.io/npm/dt/typof?label=downloads&color=%2300927F"/>
+  <img src="https://img.shields.io/npm/unpacked-size/typof?label=size&color=%2300927F"/>
+</div>
--- src/utils/Debug.util.ts
+++ src/utils/Debug.util.ts
@@ -15,16 +15,14 @@ export class Debug {
   }
 
   public info(message: string) {
-    console.log(`${this.options.scope !== undefined ? `${Styles.fg.cyan}${this.options.scope} ${Styles.fg.blue}» ` : ''}${Styles.fg.gray}[${Dayjs().format('YYYY-MM-DD HH:mm:ss')} ${Styles.fg.green}INF${Styles.fg.gray}] ${Styles.fg.green}${message}`, Styles.reset);
+    console.info(`${this.options.scope !== undefined ? `${Styles.fg.cyan}${this.options.scope} ${Styles.fg.blue}» ` : ''}${Styles.fg.gray}[${Dayjs().format('YYYY-MM-DD HH:mm:ss')} ${Styles.fg.green}INF${Styles.fg.gray}] ${Styles.fg.green}${message}`, Styles.reset);
   }
 
   public warn(message: string) {
-    console.log(`${this.options.scope !== undefined ? `${Styles.fg.cyan}${this.options.scope} ${Styles.fg.blue}» ` : ''}${Styles.fg.gray}[${Dayjs().format('YYYY-MM-DD HH:mm:ss')} ${Styles.fg.yellow}WRN${Styles.fg.gray}] ${Styles.fg.yellow}${message}`, Styles.reset);
+    console.warn(`${this.options.scope !== undefined ? `${Styles.fg.cyan}${this.options.scope} ${Styles.fg.blue}» ` : ''}${Styles.fg.gray}[${Dayjs().format('YYYY-MM-DD HH:mm:ss')} ${Styles.fg.yellow}WRN${Styles.fg.gray}] ${Styles.fg.yellow}${message}`, Styles.reset);
   }
 
   public error(message: string) {
     console.error(`${this.options.scope !== undefined ? `${Styles.fg.cyan}${this.options.scope} ${Styles.fg.blue}» ` : ''}${Styles.fg.gray}[${Dayjs().format('YYYY-MM-DD HH:mm:ss')} ${Styles.fg.red}ERR${Styles.fg.gray}] ${Styles.fg.red}${message}`, Styles.reset);
   }
 }
-
-new Debug({ scope: 'Keift' }).warn('selam');
