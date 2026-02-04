84ba782d2446c9c1a9f7234de1bdea374c2743a2
fir4tozden <fir4tozden@gmail.com>
2026-02-05 02:44:50 +0300

    v1.0.3

--- src/utils/Logger.util.ts
+++ src/utils/Logger.util.ts
@@ -14,15 +14,15 @@ export class Logger {
     this.options = merge({}, LoggerOptionsDefault, options);
   }
 
-  public info(message: string) {
+  public info(message: string): void {
     console.info(`${this.options.scope !== undefined ? `${Styles.fg.cyan}${this.options.scope} ${Styles.fg.blue}» ` : ''}${Styles.fg.gray}[${Dayjs().format('YYYY-MM-DD HH:mm:ss')} ${Styles.fg.green}INF${Styles.fg.gray}] ${Styles.fg.green}${message}`, Styles.reset);
   }
 
-  public warn(message: string) {
+  public warn(message: string): void {
     console.warn(`${this.options.scope !== undefined ? `${Styles.fg.cyan}${this.options.scope} ${Styles.fg.blue}» ` : ''}${Styles.fg.gray}[${Dayjs().format('YYYY-MM-DD HH:mm:ss')} ${Styles.fg.yellow}WRN${Styles.fg.gray}] ${Styles.fg.yellow}${message}`, Styles.reset);
   }
 
-  public error(message: string) {
+  public error(message: string): void {
     console.error(`${this.options.scope !== undefined ? `${Styles.fg.cyan}${this.options.scope} ${Styles.fg.blue}» ` : ''}${Styles.fg.gray}[${Dayjs().format('YYYY-MM-DD HH:mm:ss')} ${Styles.fg.red}ERR${Styles.fg.gray}] ${Styles.fg.red}${message}`, Styles.reset);
   }
 }
