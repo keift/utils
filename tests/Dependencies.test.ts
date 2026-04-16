7fcef4472545c9dfed5cb3da6377c37135324874
fir4tozden <fir4tozden@gmail.com>
2026-04-17 02:06:34 +0300

    v1.0.3

--- tests/Dependencies.test.ts
+++ tests/Dependencies.test.ts
@@ -1,7 +1,9 @@
 import Package from '../package.json';
 
+if (Package.dependencies.tailwindcss !== 'latest') throw new Error('❌ Error: tailwindcss');
+
 if (Package.devDependencies.prettier !== 'latest') throw new Error('❌ Error: prettier');
 if (Package.devDependencies.rulint !== 'latest') throw new Error('❌ Error: rulint');
-if (Package.devDependencies.tsup !== 'latest') throw new Error('❌ Error: tsup');
+if (Package.devDependencies.tsdown !== 'latest') throw new Error('❌ Error: tsdown');
 
 console.log('✅ Success');
