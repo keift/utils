516a7c8bfb246f1fb6d4ef924411c0ababd849f0
fir4tozden <fir4tozden@gmail.com>
2026-04-17 06:49:22 +0300

    v1.0.3

--- tests/ExportColors.test.ts
+++ tests/ExportColors.test.ts
@@ -1,6 +1,9 @@
 import fs from 'fs/promises';
 import { Colors } from '../src/main';
 
-await fs.writeFile('./assets/colors.json', JSON.stringify(Colors, undefined, 2));
+const colors_file = `${JSON.stringify(Colors, undefined, 2)}
+`;
+
+await fs.writeFile('./assets/colors.json', colors_file);
 
 console.log('✅ Success');
